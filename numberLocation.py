09ad56825e07bce1a344ac9370663cb5c771ecd1
a-robot <86808805+elithaxxor@users.noreply.github.com>
2021-11-08 16:43:46 -0800

    Update numberLocation.py

--- numberLocation.py
+++ numberLocation.py
@@ -10,6 +10,17 @@ import pandas as pd
 import folium
 import webbrowser
 import time
+import googlemaps
+from datetime import datetime
+from googleapiclient.discovery import build
+import gmaps
+#import geocoder
+import gmaps
+import gmaps.datasets
+import ipywidgets as widgets
+import geopy
+
+
 
 
 Key = '813978e32347461bb2d53b777a9545e2'
@@ -78,3 +89,89 @@ print(f'[MAP]** saved to eyespy.html')
 #pd.read_html(myMap.save('eyespy.html'))
 webbrowser.open('eyespy.html')
 webbrowser.open('/Users/macbook/Documents/CS/PROJECT/phone_number/eyespy.html')
+
+
+# ##############
+# Geocoding API	-	-	-	Details
+# Geolocation API	-	-	-	Details
+# Places API
+# ##############
+
+
+books_service = build('books', 'v1', developerKey='api_key')
+
+
+api_key = "AIzaSyCY6INDQICh3uR6yhcxdtrzZrYrzR5Lkv4"
+r"https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyCY6INDQICh3uR6yhcxdtrzZrYrzR5Lkv4"
+
+
+#phonelocate = Client(key='AIzaSyCY6INDQICh3uR6yhcxdtrzZrYrzR5Lkv4')
+#
+# # Geocoding an address
+# geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
+#
+# # Look up an address with reverse geocoding
+# reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
+#
+# # Request directions via public transit
+# now = datetime.now()
+# directions_result = gmaps.directions("Sydney Town Hall",
+#                                      "Parramatta, NSW",
+#                                      mode="transit",
+#                                      departure_time=now)
+
+
+
+
+import json
+import time
+import urllib.error
+import urllib.parse
+import urllib.request
+
+# The maps_key defined below isn't a valid Google Maps API key.
+# You need to get your own API key.
+# See https://developers.google.com/maps/documentation/timezone/get-api-key
+API_KEY = "AIzaSyCY6INDQICh3uR6yhcxdtrzZrYrzR5Lkv4"
+TIMEZONE_BASE_URL = "https://maps.googleapis.com/maps/api/timezone/json"
+API_HREF = r"https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyCY6INDQICh3uR6yhcxdtrzZrYrzR5Lkv4"
+
+
+def timezone(lat, lng, timestamp):
+    # Join the parts of the URL together into one string.
+    params = urllib.parse.urlencode(
+        {"location": f"{lat},{lng}", "timestamp": timestamp, "key": API_KEY,}
+    )
+    url = f"{TIMEZONE_BASE_URL}?{params}"
+
+    current_delay = 0.1  # Set the initial retry delay to 100ms.
+    max_delay = 5  # Set the maximum retry delay to 5 seconds.
+
+    while True:
+        try:
+            # Get the API response.
+            response = urllib.request.urlopen(url)
+        except urllib.error.URLError:
+            pass  # Fall through to the retry loop.
+        else:
+            # If we didn't get an IOError then parse the result.
+            result = json.load(response)
+            print(result)
+            if result["status"] == "OK":
+                return result["timeZoneId"]
+            elif result["status"] == "UNKNOWN_ERROR":
+                # Many API errors cannot be fixed by a retry, e.g. INVALID_REQUEST or
+                # ZERO_RESULTS. There is no point retrying these requests.
+                raise Exception(result["error_message"])
+        if current_delay > max_delay:
+            raise Exception("Too many retry attempts.")
+
+        print("Waiting", current_delay, "seconds before retrying.")
+
+        time.sleep(current_delay)
+        current_delay *= 2  # Increase the delay each time we retry.
+
+
+if __name__ == "__main__":
+    tz = timezone(39.6034810, -119.6822510, 1331161200)
+    print(f"Timezone: {tz}")
